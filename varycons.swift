dc6b2629c405bb5bf756e4de51b45645cb71e7df
Krongcc <ggchkiba@gmail.com>
2022-02-19 17:49:36 -0600

    Variables y Constantes

---
+++ varycons.swift
@@ -0,0 +1,28 @@
+// Ejemplo de usos de Variables y Constantes
+// Variables se declaran con:  var
+// Constantes se declaran con: let
+
+// Declaracion de Variables
+
+var miPrimeraVariable = "Hola Mundo"
+var miSegundaVariable = 1984
+
+print(miPrimeraVariable)
+print(miSegundaVariable)
+
+
+// Declaracion de Constantes
+
+let miPrimeraConstante = "Hola de Nuevo"
+let miSegundaConstante = 2001
+let linea = "--------------------"
+print(miPrimeraConstante)
+print(miSegundaConstante)
+print(linea, linea, linea)
+
+let suma = miSegundaVariable + miSegundaConstante
+
+print(suma)
+
+
+
