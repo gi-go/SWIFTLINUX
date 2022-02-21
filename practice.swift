3da87e24817b3971e41a75a64422dd975aad0293
Krongcc <ggchkiba@gmail.com>
2022-02-21 17:22:20 -0600

    Practicas de Variables y Constantes

---
+++ practice.swift
@@ -0,0 +1,20 @@
+/*
+  Datos del autor del libro
+  de Conan el Barbaro
+*/
+var nombre: String = "Conan"
+var titulo: String = "El Barbaro"
+var autor: String = "Robert E. Howard"
+
+let fecha: Int = 1932
+let linea: String = "----------------"
+let asterisco: String = "****************"
+
+print(linea)
+print(nombre, titulo)
+print(autor)
+print(fecha)
+print(linea)
+print(asterisco)
+print(asterisco)
+
