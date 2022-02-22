689eb504c3e4466285ae42b4c831714137b9acfe
Krongcc <ggchkiba@gmail.com>
2022-02-22 01:26:32 -0600

    Sentencias if else

--- practice.swift
+++ practice.swift
@@ -18,3 +18,21 @@ print(linea)
 print(asterisco)
 print(asterisco)
 
+// Sentencias if else if
+
+let fecha2: Int = 1905
+let fecha3: Int = 1931
+
+if (fecha2 == 1906 && fecha2 <= 1936) && fecha3 >= 1932 {
+
+  print("Robert E. Howard estaba vivo")
+
+} else if fecha3 == 1931 {
+
+  print("Es el year 1931")
+
+} else {
+  
+  print("Robert E. Howard ya habia fallecido")
+
+}
